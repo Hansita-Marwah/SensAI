8bff40c479f6c976cb4bffb9727a52726879a0ff
lucky <166947747+luckyxjx@users.noreply.github.com>
2024-04-21 00:39:40 +0530

    training.py

--- website/chatbot_working/training.py
+++ website/chatbot_working/training.py
@@ -9,48 +9,48 @@ from torch.utils.data import Dataset, DataLoader
 from utility import bag_of_words, tokenize, stem
 from my_model import NeuralNet
 
-with open('intents.json', 'r') as f:
-    intents = json.load(f)
+with open('intents.json', 'r') as l:
+    intents = json.load(l)
 
 all_words = []
 tags = []
-xy = []
+all = []
 
 for intent in intents['intents']:
     tag = intent['tag']
    
     tags.append(tag)
     for pattern in intent['patterns']:
-        w = tokenize(pattern)
+        a = tokenize(pattern)
         all_words.extend(w)
-        xy.append((w, tag))
+        all.append((w, tag))
 ignore_words = ['?', '.', '!']
-all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = [stem(a) for a in all_words if a not in ignore_words]
 
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
-print(len(xy), "patterns")
-print(len(tags), "tags:", tags)
-print(len(all_words), "unique stemmed words:", all_words)
+#print(len(all), "patterns")
+#print(len(tags), "tags:", tags)
+#print(len(all_words), all_words)
 
-X_train = []
-y_train = []
+X_way = []
+y_way = []
 for (pattern_sentence, tag) in xy:
    
     bag = bag_of_words(pattern_sentence, all_words)
-    X_train.append(bag)
+    X_way.append(bag)
     
     label = tags.index(tag)
-    y_train.append(label)
+    y_way.append(label)
 
-X_train = np.array(X_train)
-y_train = np.array(y_train)
+X_way = np.array(X_way)
+y_way = np.array(y_way)
 
 num_epochs = 1000
 batch_size = 8
 learning_rate = 0.001
-input_size = len(X_train[0])
+input_size = len(X_way[0])
 hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
@@ -58,9 +58,9 @@ print(input_size, output_size)
 class ChatDataset(Dataset):
 
     def __init__(self):
-        self.n_samples = len(X_train)
-        self.x_data = X_train
-        self.y_data = y_train
+        self.n_samples = len(X_way)
+        self.x_data = X_way
+        self.y_data = y_way
 
     
     def __getitem__(self, index):
